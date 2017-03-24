b6c0d672dc00f9c6a7822933276abe31fcf5428b
yhk <yhhklndexnug@gmail.com>
2017-03-24 15:11:57 +0800

    add

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,9 +49,9 @@ class GroupsController < ApplicationController
 
         if !current_user.is_member_of?(@group)
           current_user.join!(@group)
-          flash[:notice] = "加入本讨论版成功"
+          flash[:notice] = "收藏成功"
         else
-          flash[:warning] = "你已经是本讨论版成员了"
+          flash[:warning] = "已收藏"
       end
 
       redirect_to group_path(@group)
@@ -62,9 +62,9 @@ class GroupsController < ApplicationController
 
       if current_user.is_member_of?(@group)
         current_user.quit!(@group)
-        flash[:alert] = "已经退出本讨论版！"
+        flash[:alert] = "已取消收藏"
       else
-        flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+        flash[:warning] = "还没有收藏，怎么取消 "
       end
 
       redirect_to group_path(@group)
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ApplicationRecord
   end
 
   def quit!(group)
-    participated_groups.delete(group)    
+    participated_groups.delete(group)
   end
 end
